06d7dc5740c2f9ac30c75976d31559b60b1bc7f8
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-02-12 23:06:30 +0200

    add dockerfile, compose.yml and flyway migration

---
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,37 @@
+create table IF NOT EXISTS room_type(
+	id bigint not null primary key auto_increment,
+    type varchar(20)
+);
+
+create table IF NOT EXISTS room(
+	id bigint not null primary key auto_increment,
+    number varchar(20) unique not null,
+    price decimal(38,2) not null,
+    max_count_of_guests int not null,
+    room_type_id bigint not null,
+    foreign key (room_type_id) references room_type (id)
+);
+
+create table IF NOT EXISTS guest(
+	id bigint not null primary key auto_increment,
+    first_name varchar(50) not null,
+	last_name varchar(50) not null,
+	tel_number varchar(20) unique not null,
+    email varchar(50) unique,
+	passport_serial_number varchar(50) unique
+);
+
+create table IF NOT EXISTS reservation(
+	id bigint not null primary key auto_increment,
+    room_id bigint not null,
+	check_in_date date not null,
+	check_out_date date not null,
+	foreign key (room_id) references room (id)
+);
+
+create table IF NOT EXISTS reservations_guests(
+	reservations_id bigint not null primary key auto_increment,
+    guests_id bigint not null,
+	foreign key (reservations_id) references reservation (id),
+    foreign key (guests_id) references guest (id)
+);
