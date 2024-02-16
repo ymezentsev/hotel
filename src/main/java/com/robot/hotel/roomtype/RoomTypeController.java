3b6fb78ab7bc0947fb0bc6076383c405da466bc3
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-02-16 13:13:17 +0200

    add swagger

--- src/main/java/com/robot/hotel/roomtype/RoomTypeController.java
+++ src/main/java/com/robot/hotel/roomtype/RoomTypeController.java
@@ -1,5 +1,7 @@
 package com.robot.hotel.roomtype;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,35 +11,42 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/roomTypes")
+@Tag(name = "RoomTypes", description = "API to work with RoomTypes")
 public class RoomTypeController {
     private final RoomTypeService roomTypeService;
 
     @PostMapping()
+    @Operation(summary = "Create new room type")
     public void save(@RequestBody RoomTypeDto roomTypeDto) {
         roomTypeService.save(roomTypeDto);
     }
 
     @GetMapping()
+    @Operation(summary = "Get all room types")
     public List<RoomTypeDto> findAll() {
         return roomTypeService.findAll();
     }
 
     @GetMapping("/type/{type}")
+    @Operation(summary = "Get room type by type")
     public Optional<RoomTypeDto> findByType(@PathVariable String type) {
         return Optional.of(roomTypeService.findByType(type).orElseThrow());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get room type by id")
     public Optional<RoomTypeDto> findById(@PathVariable Long id) {
         return Optional.of(roomTypeService.findById(id).orElseThrow());
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update room type")
     public void update(@PathVariable Long id, @RequestBody RoomTypeDto roomTypeDto) {
         roomTypeService.update(id, roomTypeDto);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete room type")
     public void deleteById(@PathVariable Long id) {
         roomTypeService.deleteById(id);
     }
