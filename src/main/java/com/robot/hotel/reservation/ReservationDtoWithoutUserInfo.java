dc1805d3a1aa589753dbf104925e5fa731fa08ed
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-03-11 23:02:05 +0200

    correct printing User and Reservation

--- src/main/java/com/robot/hotel/reservation/ReservationDtoWithoutUserInfo.java
+++ src/main/java/com/robot/hotel/reservation/ReservationDtoWithoutUserInfo.java
@@ -1,19 +1,17 @@
 package com.robot.hotel.reservation;
 
-import com.robot.hotel.user.UserDtoWithoutReservation;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ReservationDto {
+public class ReservationDtoWithoutUserInfo {
     private Long id;
 
     private String roomNumber;
@@ -22,5 +20,5 @@ public class ReservationDto {
 
     private LocalDate checkOutDate;
 
-    private List<UserDtoWithoutReservation> users;
+    private Integer numberOfGuests;
 }
