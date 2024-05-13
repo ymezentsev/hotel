46196c979f18cd88f281c9d20851730c23a1e98f
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-05-13 20:56:22 +0300

    add ReservationControllerOpenApi

--- src/main/java/com/robot/hotel/reservation/ReservationController.java
+++ src/main/java/com/robot/hotel/reservation/ReservationController.java
@@ -1,7 +1,5 @@
 package com.robot.hotel.reservation;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -11,54 +9,45 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/reservations")
-@Tag(name = "Reservations", description = "API to work with Reservations")
-public class ReservationController {
+public class ReservationController implements ReservationControllerOpenApi {
     private final ReservationService reservationService;
 
     @GetMapping()
-    @Operation(summary = "Get all reservations")
     public List<ReservationDto> findAll() {
         return reservationService.findAll();
     }
 
     @GetMapping("/{id}")
-    @Operation(summary = "Get reservation by id")
     public ReservationDto findById(@PathVariable Long id) {
         return reservationService.findById(id);
     }
 
     @PostMapping()
-    @Operation(summary = "Create new reservation")
     public ReservationDto save(@Valid @RequestBody ReservationRequest reservationRequest) {
         return reservationService.save(reservationRequest);
     }
 
     @GetMapping("/user/{id}")
-    @Operation(summary = "Get reservations by user id")
     public List<ReservationDto> findReservationsByUserId(@PathVariable Long id) {
         return reservationService.findReservationsByUserId(id);
     }
 
     @GetMapping("/room/{roomNumber}")
-    @Operation(summary = "Get reservations by room's number")
     public List<ReservationDto> findReservationsByRoom(@PathVariable String roomNumber) {
         return reservationService.findReservationsByRoom(roomNumber);
     }
 
     @GetMapping("/current")
-    @Operation(summary = "Get all current reservations")
     public List<ReservationDto> findCurrentReservations() {
         return reservationService.findCurrentReservations();
     }
 
     @GetMapping("/current/room/{roomNumber}")
-    @Operation(summary = "Get all current reservations for a specific room")
     public List<ReservationDto> findCurrentReservationsForSpecificRoom(@PathVariable String roomNumber) {
         return reservationService.findCurrentReservationsForSpecificRoom(roomNumber);
     }
 
     @DeleteMapping("/{id}")
-    @Operation(summary = "Delete reservation")
     public void deleteById(@PathVariable Long id) {
         reservationService.deleteById(id);
     }
---
+++ src/main/java/com/robot/hotel/reservation/ReservationControllerOpenApi.java
@@ -0,0 +1,120 @@
+package com.robot.hotel.reservation;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Tag(name = "Reservations Controller", description = "API to work with Reservations")
+public interface ReservationControllerOpenApi {
+    @Operation(summary = "Get all reservations")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully fetched all reservations")
+    })
+    List<ReservationDto> findAll();
+
+    @Operation(summary = "Get reservation by id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully fetched reservation"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Such reservation is not exists")
+    })
+    ReservationDto findById(@PathVariable Long id);
+
+    @Operation(summary = "Create new reservation")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Reservation successfully created",
+                    content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema =
+                                    @Schema(implementation = ReservationRequest.class))
+                    }),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Such room is not exists"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Such user is not exists"),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Check in date must be before check out date"),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "You can't reserve room for more than 60 days"),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Reservation of rooms opens 180 days in advance"),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "The quantity of guests exceeds the maximum allowed in this room"),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "This room is occupied for your dates")
+    })
+    ReservationDto save(@Valid @RequestBody ReservationRequest reservationRequest);
+
+    @Operation(summary = "Get reservations by user id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully fetched reservations by user id")
+    })
+    List<ReservationDto> findReservationsByUserId(@PathVariable Long id);
+
+    @Operation(summary = "Get reservations by room's number")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully fetched reservations by room's number"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Such room is not exists")
+    })
+    List<ReservationDto> findReservationsByRoom(@PathVariable String roomNumber);
+
+    @Operation(summary = "Get all current reservations")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully fetched all current reservations")
+    })
+    List<ReservationDto> findCurrentReservations();
+
+    @Operation(summary = "Get all current reservations for a specific room")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully fetched all current reservations for a specific room"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Such room is not exists")
+    })
+    List<ReservationDto> findCurrentReservationsForSpecificRoom(@PathVariable String roomNumber);
+
+    @Operation(summary = "Delete reservation")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully deleted reservation"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Such reservation is not exists")
+    })
+    void deleteById(@PathVariable Long id);
+}
