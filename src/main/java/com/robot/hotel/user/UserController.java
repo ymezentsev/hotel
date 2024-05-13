c80ed29824b1e7842803c8cd7239f60d3543dfed
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-05-13 21:16:49 +0300

    add UserControllerOpenApi

--- src/main/java/com/robot/hotel/user/UserController.java
+++ src/main/java/com/robot/hotel/user/UserController.java
@@ -1,7 +1,5 @@
 package com.robot.hotel.user;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -11,73 +9,60 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
-@Tag(name = "Users", description = "API to work with Users")
-public class UserController {
-
+public class UserController implements UserControllerOpenApi {
     private final UserService userService;
 
     @GetMapping()
-    @Operation(summary = "Get all users")
     public List<UserDto> findAll() {
         return userService.findAll();
     }
 
     @PostMapping()
-    @Operation(summary = "Create new user")
     public UserDto save(@Valid @RequestBody UserRequest userRequest) {
         return userService.save(userRequest);
     }
 
     @GetMapping(value = "/{id}")
-    @Operation(summary = "Get user by id")
     public UserDto findById(@PathVariable Long id) {
         return userService.findById(id);
     }
 
     @GetMapping("/email/{email}")
-    @Operation(summary = "Get user by email")
     public UserDto findByEmail(@PathVariable String email) {
         return userService.findByEmail(email);
     }
 
     @GetMapping("/telNumber/{telNumber}")
-    @Operation(summary = "Get user by tel. number")
     public UserDto findByTelNumber(@PathVariable String telNumber) {
         return userService.findByTelNumber(telNumber);
     }
 
     @GetMapping("/passport/{passportSerialNumber}")
-    @Operation(summary = "Get user by passport number")
     public UserDto findByPassportSerialNumber(@PathVariable String passportSerialNumber) {
         return userService.findByPassportSerialNumber(passportSerialNumber);
     }
 
     @GetMapping("/lastName/{lastName}")
-    @Operation(summary = "Get users by lastname")
     public List<UserDto> findByLastName(@PathVariable String lastName) {
         return userService.findByLastName(lastName);
     }
 
     @GetMapping("/reservations/{id}")
-    @Operation(summary = "Get users by reservation")
     public List<UserDto> findUsersByReservation(@PathVariable Long id) {
         return userService.findUsersByReservation(id);
     }
 
     @GetMapping("/role/{role}")
-    @Operation(summary = "Get users by role")
     public List<UserDto> findUsersByRole(@PathVariable String role) {
         return userService.findUsersByRole(role);
     }
 
     @PutMapping("/{id}")
-    @Operation(summary = "Update user")
     public void update(@PathVariable Long id, @Valid @RequestBody UserRequest userRequest) {
         userService.update(id, userRequest);
     }
 
     @DeleteMapping("/{id}")
-    @Operation(summary = "Delete user")
     public void deleteById(@PathVariable Long id) {
         userService.deleteById(id);
     }
---
+++ src/main/java/com/robot/hotel/user/UserControllerOpenApi.java
@@ -0,0 +1,158 @@
+package com.robot.hotel.user;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Tag(name = "Users Controller", description = "API to work with Users")
+public interface UserControllerOpenApi {
+
+    @Operation(summary = "Get all users")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully fetched all users")
+    })
+    List<UserDto> findAll();
+
+    @Operation(summary = "Create new user")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "User successfully created",
+                    content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema =
+                                    @Schema(implementation = UserRequest.class))
+                    }),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Such user is already exists"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Such phone code is not exists"),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "There is not enough information to save user's passport")
+    })
+    UserDto save(@Valid @RequestBody UserRequest userRequest);
+
+    @Operation(summary = "Get user by id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully fetched user by id"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Such user is not exists")
+    })
+    UserDto findById(@PathVariable Long id);
+
+    @Operation(summary = "Get user by email")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully fetched user by email"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Such user is not exists")
+    })
+    UserDto findByEmail(@PathVariable String email);
+
+    @Operation(summary = "Get user by phone number")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully fetched user by phone number"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Such user is not exists")
+    })
+    UserDto findByTelNumber(@PathVariable String telNumber);
+
+    @Operation(summary = "Get user by passport number")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully fetched user by passport number"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Such user is not exists")
+    })
+    UserDto findByPassportSerialNumber(@PathVariable String passportSerialNumber);
+
+    @Operation(summary = "Get users by lastname")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully fetched users by lastname")
+    })
+    List<UserDto> findByLastName(@PathVariable String lastName);
+
+    @Operation(summary = "Get users by reservation")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully fetched users by reservation")
+    })
+    List<UserDto> findUsersByReservation(@PathVariable Long id);
+
+    @Operation(summary = "Get users by role")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully fetched users by role"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Such role is not exists")
+    })
+    List<UserDto> findUsersByRole(@PathVariable String role);
+
+    @Operation(summary = "Update user")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully updated user",
+                    content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema =
+                                    @Schema(implementation = UserRequest.class))
+                    }),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Such user is already exists"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Such user is not exists"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Such phone code is not exists")
+    })
+    void update(@PathVariable Long id, @Valid @RequestBody UserRequest userRequest);
+
+    @Operation(summary = "Delete user")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successfully deleted user"),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Failed to delete user. " +
+                            "This user has reservations. At first delete reservations"),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Such user is not exists")
+    })
+    void deleteById(@PathVariable Long id);
+}
