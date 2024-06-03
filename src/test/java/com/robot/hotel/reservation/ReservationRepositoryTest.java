943cf53dcaaecfc601a75e63bc1ed3c854ce8fbc
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-06-03 23:28:35 +0300

    add parametrized tests for ReservationRepositoryTest

--- src/test/java/com/robot/hotel/reservation/ReservationRepositoryTest.java
+++ src/test/java/com/robot/hotel/reservation/ReservationRepositoryTest.java
@@ -3,6 +3,8 @@ package com.robot.hotel.reservation;
 import com.robot.hotel.ContainerConfiguration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +12,6 @@ import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @AutoConfigureDataJpa
@@ -19,45 +20,40 @@ class ReservationRepositoryTest {
     @Autowired
     ReservationRepository reservationRepository;
 
-    @Test
+    @ParameterizedTest
     @DisplayName("Find reservations by user id")
-    void findByUserIdTest() {
-        assertAll(
-                () -> assertEquals(2,
-                        reservationRepository.findByUsersId(2L, Pageable.unpaged()).getTotalElements()),
-                () -> assertEquals(3,
-                        reservationRepository.findByUsersId(3L, Pageable.unpaged()).getTotalElements()),
-                () -> assertEquals(0,
-                        reservationRepository.findByUsersId(4L, Pageable.unpaged()).getTotalElements())
-        );
+    @CsvSource(value = {
+            "2, 2",
+            "3, 3",
+            "4, 0"
+    })
+    void findByUserIdTest(Long userId, int result) {
+        assertEquals(result,
+                reservationRepository.findByUsersId(userId, Pageable.unpaged()).getTotalElements());
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("Find reservations by room id")
-    void findByRoomIdTest() {
-        assertAll(
-                () -> assertEquals(1,
-                        reservationRepository.findByRoomId(1L, Pageable.unpaged()).getTotalElements()),
-                () -> assertEquals(2,
-                        reservationRepository.findByRoomId(5L, Pageable.unpaged()).getTotalElements()),
-                () -> assertEquals(0,
-                        reservationRepository.findByRoomId(3L, Pageable.unpaged()).getTotalElements())
-        );
+    @CsvSource(value = {
+            "1, 1",
+            "5, 2",
+            "3, 0"
+    })
+    void findByRoomIdTest(Long roomId, int result) {
+        assertEquals(result,
+                reservationRepository.findByRoomId(roomId, Pageable.unpaged()).getTotalElements());
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("Find free rooms for which reservations exist")
-    void findFreeRoomsWithReservationsTest() {
-        assertAll(
-                () -> assertEquals(1, reservationRepository
-                        .findFreeRoomsWithReservations(LocalDate.now(), LocalDate.now().plusDays(1)).size()),
-                () -> assertEquals(2, reservationRepository
-                        .findFreeRoomsWithReservations(LocalDate.now().plusDays(3),
-                                LocalDate.now().plusDays(4)).size()),
-                () -> assertEquals(3, reservationRepository
-                        .findFreeRoomsWithReservations(LocalDate.now().plusDays(6),
-                                LocalDate.now().plusDays(7)).size())
-        );
+    @CsvSource(value = {
+            "1, 1",
+            "3, 2",
+            "6, 3"
+    })
+    void findFreeRoomsWithReservationsTest(int days, int result) {
+        assertEquals(result, reservationRepository
+                .findFreeRoomsWithReservations(LocalDate.now(), LocalDate.now().plusDays(days)).size());
     }
 
     @Test
@@ -67,16 +63,15 @@ class ReservationRepositoryTest {
                 reservationRepository.findCurrentReservations(Pageable.unpaged()).getTotalElements());
     }
 
-    @Test
+    @ParameterizedTest
     @DisplayName("Find current reservations for specific room")
-    void findCurrentReservationsForRoomTest() {
-        assertAll(
-                () -> assertEquals(1, reservationRepository
-                        .findCurrentReservationsForRoom(5L, Pageable.unpaged()).getTotalElements()),
-                () -> assertEquals(1, reservationRepository
-                        .findCurrentReservationsForRoom(4L, Pageable.unpaged()).getTotalElements()),
-                () -> assertEquals(0, reservationRepository
-                        .findCurrentReservationsForRoom(2L, Pageable.unpaged()).getTotalElements())
-        );
+    @CsvSource(value = {
+            "5, 1",
+            "4, 1",
+            "2, 0"
+    })
+    void findCurrentReservationsForRoomTest(Long roomId, int result) {
+        assertEquals(result, reservationRepository
+                .findCurrentReservationsForRoom(roomId, Pageable.unpaged()).getTotalElements());
     }
 }
