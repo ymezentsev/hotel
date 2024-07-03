ba827b3c7cc51cb1521ddf0e8e621582641a9c76
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-07-03 20:07:37 +0300

    add tests for CountryRepository

---
+++ src/test/java/com/robot/hotel/user/country/CountryRepositoryTest.java
@@ -0,0 +1,21 @@
+package com.robot.hotel.user.country;
+
+import com.robot.hotel.ContainerConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@AutoConfigureDataJpa
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ContainerConfiguration.class)
+class CountryRepositoryTest {
+    @Autowired
+    CountryRepository countryRepository;
+
+    @Test
+    void findByPhoneCodeTest() {
+        assertEquals("UKR", countryRepository.findByPhoneCode("+380").get(0).getCountryCode());
+    }
+}
