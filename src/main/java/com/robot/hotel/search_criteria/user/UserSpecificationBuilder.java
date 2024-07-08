13947eb73d1fbd773826e17de7d1610aff4afd8c
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-07-08 09:13:07 +0300

    add UserSpecificationBuilder

---
+++ src/main/java/com/robot/hotel/search_criteria/user/UserSpecificationBuilder.java
@@ -0,0 +1,85 @@
+package com.robot.hotel.search_criteria.user;
+
+import com.robot.hotel.search_criteria.SpecificationBuilder;
+import com.robot.hotel.search_criteria.SpecificationProviderManager;
+import com.robot.hotel.user.User;
+import com.robot.hotel.user.dto.UserSearchParameters;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public abstract class UserSpecificationBuilder implements SpecificationBuilder<User> {
+    private final SpecificationProviderManager<User> specificationProviderManager;
+
+    @Override
+    public Specification<User> build(UserSearchParameters searchParameters) {
+        Specification<User> spec = Specification.where(null);
+
+        if(searchParameters != null
+                && searchParameters.firstnames() != null
+                && searchParameters.firstnames().length > 0){
+            spec = spec.and(specificationProviderManager
+                    .getSpecificationProvider("firstname")
+                    .getSpecification(searchParameters.firstnames()));
+        }
+
+        if(searchParameters != null
+                && searchParameters.lastnames() != null
+                && searchParameters.lastnames().length > 0){
+            spec = spec.and(specificationProviderManager
+                    .getSpecificationProvider("lastname")
+                    .getSpecification(searchParameters.lastnames()));
+        }
+
+        if(searchParameters != null
+                && searchParameters.phoneNumbers() != null
+                && searchParameters.phoneNumbers().length > 0){
+            spec = spec.and(specificationProviderManager
+                    .getSpecificationProvider("phoneNumber")
+                    .getSpecification(searchParameters.phoneNumbers()));
+        }
+
+        if(searchParameters != null
+                && searchParameters.emails() != null
+                && searchParameters.emails().length > 0){
+            spec = spec.and(specificationProviderManager
+                    .getSpecificationProvider("email")
+                    .getSpecification(searchParameters.emails()));
+        }
+
+        if(searchParameters != null
+                && searchParameters.roles() != null
+                && searchParameters.roles().length > 0){
+            spec = spec.and(specificationProviderManager
+                    .getSpecificationProvider("role")
+                    .getSpecification(searchParameters.roles()));
+        }
+
+        if(searchParameters != null
+                && searchParameters.passportSerialNumbers() != null
+                && searchParameters.passportSerialNumbers().length > 0){
+            spec = spec.and(specificationProviderManager
+                    .getSpecificationProvider("passportSerialNumber")
+                    .getSpecification(searchParameters.passportSerialNumbers()));
+        }
+
+        if(searchParameters != null
+                && searchParameters.reservations() != null
+                && searchParameters.reservations().length > 0){
+            spec = spec.and(specificationProviderManager
+                    .getSpecificationProvider("reservation")
+                    .getSpecification(searchParameters.reservations()));
+        }
+
+        if(searchParameters != null
+                && searchParameters.countryCodes() != null
+                && searchParameters.countryCodes().length > 0){
+            spec = spec.and(specificationProviderManager
+                    .getSpecificationProvider("countryCode")
+                    .getSpecification(searchParameters.countryCodes()));
+        }
+        return spec;
+    }
+ }
---
+++ src/main/java/com/robot/hotel/search_criteria/user/UserSpecificationProviderManager.java
@@ -0,0 +1,24 @@
+package com.robot.hotel.search_criteria.user;
+
+import com.robot.hotel.search_criteria.SpecificationProvider;
+import com.robot.hotel.search_criteria.SpecificationProviderManager;
+import com.robot.hotel.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class UserSpecificationProviderManager implements SpecificationProviderManager<User> {
+    private final List<SpecificationProvider<User>> requestSpecificationProviders;
+
+    @Override
+    public SpecificationProvider<User> getSpecificationProvider(String key) {
+        return requestSpecificationProviders.stream()
+                .filter(provider -> provider.getKey().equals(key))
+                .findFirst()
+                .orElseThrow(() ->
+                        new RuntimeException("Can't find correct specification provider for key " + key));
+    }
+}
