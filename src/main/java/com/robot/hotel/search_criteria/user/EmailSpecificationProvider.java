f10ebd1a8a4b9530a2edd42b42bad983be8b0ff4
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-07-09 09:51:26 +0300

    add search by email

---
+++ src/main/java/com/robot/hotel/search_criteria/user/EmailSpecificationProvider.java
@@ -0,0 +1,25 @@
+package com.robot.hotel.search_criteria.user;
+
+import com.robot.hotel.search_criteria.SpecificationProvider;
+import com.robot.hotel.user.User;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class EmailSpecificationProvider implements SpecificationProvider<User> {
+    @Override
+    public String getKey() {
+        return "email";
+    }
+
+    @Override
+    public Specification<User> getSpecification(String[] params) {
+        return (root, query, criteriaBuilder) -> root
+                .get("email")
+                .in(Arrays.stream(params)
+                        .map(name -> name.toLowerCase().strip())
+                        .toArray());
+    }
+}
