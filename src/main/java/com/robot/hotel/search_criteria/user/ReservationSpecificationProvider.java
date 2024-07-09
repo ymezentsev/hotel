2a56bb0274ac4c5255c8660c0b9cf05e1d609a30
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-07-09 09:48:50 +0300

    add search by reservations

---
+++ src/main/java/com/robot/hotel/search_criteria/user/ReservationSpecificationProvider.java
@@ -0,0 +1,26 @@
+package com.robot.hotel.search_criteria.user;
+
+import com.robot.hotel.search_criteria.SpecificationProvider;
+import com.robot.hotel.user.User;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class ReservationSpecificationProvider implements SpecificationProvider<User> {
+    @Override
+    public String getKey() {
+        return "reservation";
+    }
+
+    @Override
+    public Specification<User> getSpecification(String[] params) {
+        return (root, query, criteriaBuilder) -> {
+            query.distinct(true);
+            return root.get("reservations")
+                    .get("id")
+                    .in(Arrays.stream(params).toArray());
+        };
+    }
+}
