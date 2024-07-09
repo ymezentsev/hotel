33e983c9e411192688f549e9dd6347aa30a08db7
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-07-09 09:49:40 +0300

    add search by country code

---
+++ src/main/java/com/robot/hotel/search_criteria/user/CountryCodeSpecificationProvider.java
@@ -0,0 +1,27 @@
+package com.robot.hotel.search_criteria.user;
+
+import com.robot.hotel.search_criteria.SpecificationProvider;
+import com.robot.hotel.user.User;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class CountryCodeSpecificationProvider implements SpecificationProvider<User> {
+    @Override
+    public String getKey() {
+        return "countryCode";
+    }
+
+    @Override
+    public Specification<User> getSpecification(String[] params) {
+        return (root, query, criteriaBuilder) -> root
+                .get("passport")
+                .get("country")
+                .get("countryCode")
+                .in(Arrays.stream(params)
+                        .map(name -> name.toUpperCase().strip())
+                        .toArray());
+    }
+}
