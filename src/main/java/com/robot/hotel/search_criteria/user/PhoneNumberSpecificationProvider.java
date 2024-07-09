d4481ae36830dd315d62b0c4a73f0a89febc6846
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-07-09 17:33:55 +0300

    add search by short and full phone number

---
+++ src/main/java/com/robot/hotel/search_criteria/user/PhoneNumberSpecificationProvider.java
@@ -0,0 +1,41 @@
+package com.robot.hotel.search_criteria.user;
+
+import com.robot.hotel.search_criteria.SpecificationProvider;
+import com.robot.hotel.user.User;
+import jakarta.persistence.criteria.Predicate;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class PhoneNumberSpecificationProvider implements SpecificationProvider<User> {
+    @Override
+    public String getKey() {
+        return "phoneNumber";
+    }
+
+    @Override
+    public Specification<User> getSpecification(String[] params) {
+        return (root, query, criteriaBuilder) -> {
+            List<Predicate> fullPhoneNumPredicates  = Arrays.stream(params)
+                    .filter(param -> param.startsWith("+"))
+                    .map(param -> criteriaBuilder.like(
+                            criteriaBuilder.concat(root.get("country").get("phoneCode"), root.get("phoneNumber")),
+                            "%" + param.strip() + "%"))
+                    .toList();
+
+            List<Predicate> shortPhoneNumPredicates = Arrays.stream(params)
+                    .filter(param -> !param.startsWith("+"))
+                    .map(param -> criteriaBuilder.like(root.get("phoneNumber"),
+                            "%" + param.strip() + "%"))
+                    .toList();
+
+            List<Predicate> allPhoneNumPredicates = new ArrayList<>(fullPhoneNumPredicates);
+            allPhoneNumPredicates.addAll(shortPhoneNumPredicates);
+            return criteriaBuilder.or(allPhoneNumPredicates.toArray(new Predicate[0]));
+        };
+    }
+}
