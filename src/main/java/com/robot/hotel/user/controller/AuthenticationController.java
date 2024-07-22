21e5ea78e8221978f67767743147641bdce96eb4
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-07-23 00:52:26 +0300

    update AuthenticationControllerTest

--- src/main/java/com/robot/hotel/user/controller/AuthenticationController.java
+++ src/main/java/com/robot/hotel/user/controller/AuthenticationController.java
@@ -5,10 +5,10 @@ import com.robot.hotel.user.dto.UserDto;
 import com.robot.hotel.user.service.RegistrationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -16,8 +16,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private final RegistrationService registrationService;
 
-    @PostMapping()
+    @PostMapping("/register")
     public UserDto register(@Valid @RequestBody RegistrationRequestDto registrationRequestDto) {
         return registrationService.register(registrationRequestDto);
+    }
+
+    @GetMapping("/confirm")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Void> confirm(@RequestParam("token") String token) {
+        HttpHeaders headers = new HttpHeaders();
+
+/*        try {
+            registrationService.confirmToken(token);
+            headers.add("Location", frontendBaseUrl + LOGIN_PAGE_URL);
+        } catch (EmailConfirmationTokenExpiredException e) {
+            headers.add("Location",
+                    frontendBaseUrl + AUTH_ERROR_PAGE_URL + e.getMessage().substring(14));
+        } catch (EmailAlreadyConfirmedException e) {
+            headers.add("Location", frontendBaseUrl + LOGIN_PAGE_URL);
+        }*/
+
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 }
