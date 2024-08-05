8b9e29f61e295a256053a324355b9aed943fa747
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-08-05 12:43:15 +0300

    update flyway migration

--- src/main/resources/db/migration/V3__add_admin_to_db.sql
+++ src/main/resources/db/migration/V3__add_admin_to_db.sql
@@ -1,2 +1,4 @@
 INSERT INTO users (first_name, last_name, phone_country_code, phone_number, email, password, role, passport_id, is_enabled)
-VALUES  ('admin', 'admin', 'UKR', '991111111', 'admin@gmail.com', 'User1User1', 'ADMIN', NULL, true);
+values  ('admin', 'admin', 'UKR', '991111111', 'admin@gmail.com',
+'$2a$12$owtTS8Q5teMgBiUMju1cy.NNDMGUhEKnelNJ8uL2Q/4FsvFg7/6Yq', 'ADMIN', null, true);
+-- admin@gmail.com          |   password: Admin123
