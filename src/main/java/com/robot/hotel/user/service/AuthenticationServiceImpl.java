b10e684d7971374e7c7ddd0f861ef316b35f5e82
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-08-14 18:04:34 +0300

    add logger to AuthenticationService

--- src/main/java/com/robot/hotel/user/service/AuthenticationServiceImpl.java
+++ src/main/java/com/robot/hotel/user/service/AuthenticationServiceImpl.java
@@ -4,12 +4,14 @@ import com.robot.hotel.security.JwtUtil;
 import com.robot.hotel.user.dto.login.LoginRequestDto;
 import com.robot.hotel.user.dto.login.LoginResponseDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
@@ -17,14 +19,15 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final AuthenticationManager authenticationManager;
 
     //todo add test
-    //todo add logger
     @Override
     public LoginResponseDto authenticate(LoginRequestDto request) {
+        log.info("Authenticating user with email: {}", request.getEmail());
         Authentication authentication = authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(
                         request.getEmail().toLowerCase(), request.getPassword()));
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
+        log.info("User {} successfully authenticated", request.getEmail());
         return new LoginResponseDto(jwtUtil.generateToken(request.getEmail()));
     }
 }
