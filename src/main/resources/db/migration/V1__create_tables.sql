c1e67001d5dc93dbd7770621577697089b287819
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-08-16 17:12:51 +0300

    update DB (add tables "roles" and "users_roles")

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -34,11 +34,23 @@ CREATE TABLE IF NOT EXISTS users (
 	phone_number VARCHAR(12) UNIQUE NOT NULL,
     email VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL,
-    role VARCHAR(10) NOT NULL DEFAULT 'USER',
 	passport_id BIGINT,
 	is_enabled BOOLEAN NOT NULL,
 	FOREIGN KEY (passport_id) REFERENCES passport (id),
 	FOREIGN KEY (phone_country_code) REFERENCES country (country_code)
+);
+
+CREATE TABLE IF NOT EXISTS roles (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    role_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS users_roles (
+    user_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id)
 );
 
 CREATE TABLE IF NOT EXISTS reservation (
--- src/main/resources/db/migration/V3__add_admin_to_db.sql
+++ src/main/resources/db/migration/V3__add_admin_to_db.sql
@@ -1,4 +1,12 @@
-INSERT INTO users (first_name, last_name, phone_country_code, phone_number, email, password, role, passport_id, is_enabled)
+INSERT INTO users (first_name, last_name, phone_country_code, phone_number, email, password, passport_id, is_enabled)
 values  ('admin', 'admin', 'UKR', '991111111', 'admin@gmail.com',
-'$2a$12$owtTS8Q5teMgBiUMju1cy.NNDMGUhEKnelNJ8uL2Q/4FsvFg7/6Yq', 'ADMIN', null, true);
+'$2a$12$owtTS8Q5teMgBiUMju1cy.NNDMGUhEKnelNJ8uL2Q/4FsvFg7/6Yq', null, true);
 -- admin@gmail.com          |   password: Admin123
+
+INSERT INTO roles (role_name)
+VALUES ('USER'),
+       ('MANAGER'),
+       ('ADMIN');
+
+INSERT INTO users_roles (user_id, role_id)
+VALUES (1, 3);
