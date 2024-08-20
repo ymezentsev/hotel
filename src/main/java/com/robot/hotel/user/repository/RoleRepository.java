3fb21e409155ff66b1d5da1e18f6a99f5016c90e
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-08-20 18:20:30 +0300

    add repository for roles

---
+++ src/main/java/com/robot/hotel/user/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.robot.hotel.user.repository;
+
+import com.robot.hotel.user.model.Role;
+import com.robot.hotel.user.model.enums.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    //todo add tests
+    Optional<Role> findByRoleName(RoleName role);
+}
