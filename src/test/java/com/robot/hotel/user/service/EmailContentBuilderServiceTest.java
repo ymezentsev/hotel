cc002406ba0fa08d01b6691280b0dbcd0e13588f
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-08-21 11:56:56 +0300

    add tests for EmailContentBuilderService

--- src/test/java/com/robot/hotel/user/service/EmailContentBuilderServiceTest.java
+++ src/test/java/com/robot/hotel/user/service/EmailContentBuilderServiceTest.java
@@ -2,7 +2,6 @@ package com.robot.hotel.user.service;
 
 import com.robot.hotel.ContainerConfiguration;
 import com.robot.hotel.user.model.enums.EmailSubject;
-import com.robot.hotel.user.service.impl.EmailSenderServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,9 +13,9 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = ContainerConfiguration.class)
-class EmailSenderServiceImplTest {
+class EmailContentBuilderServiceTest {
     @Autowired
-    EmailSenderServiceImpl emailSenderService;
+    EmailContentBuilderService emailContentBuilderService;
 
     @ParameterizedTest
     @CsvSource(value = {
@@ -25,13 +24,13 @@ class EmailSenderServiceImplTest {
     })
     @DisplayName("Successful build email content")
     void buildEmailContentTest(EmailSubject subject, String result) {
-        assertTrue(emailSenderService.buildEmailContent("test", "test", subject).contains(result));
+        assertTrue(emailContentBuilderService.buildEmailContent("test", "test", subject).contains(result));
     }
 
     @Test
     @DisplayName("Fail build email content")
     void buildEmailContentThrowIllegalArgumentExceptionTest() {
         assertThrows(IllegalArgumentException.class,
-                () -> emailSenderService.buildEmailContent("test", "test", EmailSubject.valueOf("TEST")));
+                () -> emailContentBuilderService.buildEmailContent("test", "test", EmailSubject.valueOf("TEST")));
     }
 }
