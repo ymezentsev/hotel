a492783bd132ba73ed86bde4bc491b124a62c068
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-09-03 16:18:29 +0300

    add ToStringExclude to Country entity

--- src/main/java/com/robot/hotel/country/Country.java
+++ src/main/java/com/robot/hotel/country/Country.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ import java.util.List;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = {"passports", "users"})
 public class Country {
     @Id
     private String countryCode;
