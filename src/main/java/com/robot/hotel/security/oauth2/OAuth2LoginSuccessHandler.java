30872c64436e7a82776a6ca856c48a9511d80490
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-09-04 21:45:32 +0300

    add logger to OAuth2LoginSuccessHandler

--- src/main/java/com/robot/hotel/security/oauth2/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/robot/hotel/security/oauth2/OAuth2LoginSuccessHandler.java
@@ -5,23 +5,23 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     private final CustomOAuth2UserService customOAuth2UserService;
-/*    private final JwtUtil jwtUtil;
-    @Value("${frontend.base.url}")
+    // private final JwtUtil jwtUtil;
+/*    @Value("${frontend.base.url}")
     String frontendBaseUrl;*/
 
     @Override
-    //todo add logger
-    //todo add test
     //todo add frontend page to receive token
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
@@ -29,8 +29,10 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
         User loggedInUser = customOAuth2UserService.registerNewUserAfterOAuthLogin(oAuth2User);
 
-/*        String token = jwtUtil.generateToken(loggedInUser.getEmail());
-        response.sendRedirect(frontendBaseUrl + "?token=" + token);*/
+        log.info("Google attributes of the logged in user: {}", oAuth2User.getAttributes().toString());
+
+        //String token = jwtUtil.generateToken(loggedInUser.getEmail());
+        //response.sendRedirect(frontendBaseUrl + "?token=" + token);
         super.onAuthenticationSuccess(request, response, authentication);
     }
 }
