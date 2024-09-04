3356d5283a028b75219cf3eaf53874c42be02ffe
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-09-04 21:32:45 +0300

    update OAuth2TestController for check OAuth2 authorization

--- src/main/java/com/robot/hotel/user/controller/OAuth2TestController.java
+++ src/main/java/com/robot/hotel/user/controller/OAuth2TestController.java
@@ -1,12 +1,18 @@
 package com.robot.hotel.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-//todo update test controller
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1/google")
 @RequiredArgsConstructor
 public class OAuth2TestController {
+
+    @GetMapping()
+    public ModelAndView register() {
+       return new ModelAndView("google-login");
+    }
 }
