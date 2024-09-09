e0a6809d9e2dc3004020509b320ae7639d612e46
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-09-09 20:03:39 +0300

    update DuplicateObjectException (set ResponseStatus)

--- src/main/java/com/robot/hotel/exception/DuplicateObjectException.java
+++ src/main/java/com/robot/hotel/exception/DuplicateObjectException.java
@@ -1,5 +1,9 @@
 package com.robot.hotel.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class DuplicateObjectException extends RuntimeException {
     public DuplicateObjectException(String message) {
         super(message);
