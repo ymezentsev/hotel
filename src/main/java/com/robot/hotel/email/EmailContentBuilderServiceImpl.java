0c6523d7c8561c0227929091543daf745e3cb16e
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-09-20 20:40:06 +0300

    update EmailContentBuilderServiceImpl (set context for reservation)

--- src/main/java/com/robot/hotel/email/EmailContentBuilderServiceImpl.java
+++ src/main/java/com/robot/hotel/email/EmailContentBuilderServiceImpl.java
@@ -8,6 +8,11 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.math.BigDecimal;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Locale;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -49,7 +54,17 @@ public class EmailContentBuilderServiceImpl implements EmailContentBuilderServic
         }
 
         if (reservation != null) {
-            //ctx.setVariable("reservation", reservation);
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy, EEE").withLocale(Locale.US);
+            BigDecimal totalPrice = reservation.getRoom().getPrice()
+                    .multiply(BigDecimal.valueOf(ChronoUnit.DAYS.between(reservation.getCheckInDate(), reservation.getCheckOutDate())));
+
+           ctx.setVariable("checkIn", reservation.getCheckInDate().format(dateTimeFormatter) + " (14:00)");
+            ctx.setVariable("checkOut", reservation.getCheckOutDate().format(dateTimeFormatter) + " (12:00)");
+            ctx.setVariable("room", "№" + reservation.getRoom().getNumber()
+                    + ", " + reservation.getRoom().getRoomType().getType());
+            ctx.setVariable("guestsQuantity", reservation.getUsers().size());
+            ctx.setVariable("price", reservation.getRoom().getPrice());
+            ctx.setVariable("totalPrice", totalPrice);
         }
         return htmlTemplateEngine.process(template, ctx);
     }
