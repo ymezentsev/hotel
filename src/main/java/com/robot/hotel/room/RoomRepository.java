e79792fca2193dce077bae7c43c5194d931c31c2
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-09-23 00:19:17 +0300

    update findFreeRooms method at RoomRepository

--- src/main/java/com/robot/hotel/room/RoomRepository.java
+++ src/main/java/com/robot/hotel/room/RoomRepository.java
@@ -3,9 +3,11 @@ package com.robot.hotel.room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +25,17 @@ public interface RoomRepository extends JpaRepository<Room, Long>, JpaSpecificat
             WHERE r.id = :id""")
     Optional<Room> findById(@NonNull Long id);
 
-    @Query("SELECT r FROM Room r LEFT JOIN Reservation res ON r.id = res.room.id WHERE room.id IS NULL")
-    List<Room> findRoomsWithoutReservations();
+    @Query("""
+            SELECT rm FROM Room rm
+            LEFT JOIN FETCH rm.reservations r
+            WHERE rm NOT IN (
+                SELECT DISTINCT r.room FROM Reservation r
+                WHERE ((r.checkInDate <= :checkIn AND :checkIn < r.checkOutDate)
+                OR (r.checkInDate < :checkOut AND :checkOut <= r.checkOutDate)
+                OR (:checkIn <= r.checkInDate AND r.checkOutDate <= :checkOut))
+                AND r.checkOutDate > CURDATE()
+            )
+            """)
+    List<Room> findFreeRooms(@Param("checkIn") LocalDate checkIn,
+                             @Param("checkOut") LocalDate checkOut);
 }
