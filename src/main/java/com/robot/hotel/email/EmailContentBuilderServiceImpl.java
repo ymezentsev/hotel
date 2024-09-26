52e913235e1f8c28b9e01fef189304df8961c9de
Yurii Mezentsev <yvmezentsev@gmail.com>
2024-09-26 17:27:12 +0300

    update EmailContentBuilderServiceImpl (delete case)

--- src/main/java/com/robot/hotel/email/EmailContentBuilderServiceImpl.java
+++ src/main/java/com/robot/hotel/email/EmailContentBuilderServiceImpl.java
@@ -3,7 +3,6 @@ package com.robot.hotel.email;
 import com.robot.hotel.reservation.Reservation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -19,33 +18,10 @@ import java.util.Locale;
 public class EmailContentBuilderServiceImpl implements EmailContentBuilderService {
     private final TemplateEngine htmlTemplateEngine;
 
-    private static final String TOKEN_CONFIRMATION_EMAIL_URL = "/api/v1/auth/confirm?token=";
-    private static final String TOKEN_RESET_PASSWORD_URL = "/api/v1/users/reset-password?token=";
-    private static final String TEMPLATE_FOR_CONFIRM_EMAIL = "confirm-email";
-    private static final String TEMPLATE_FOR_FORGOT_PASSWORD_EMAIL = "forgot-password-email";
-    private static final String TEMPLATE_FOR_RESERVATION_CONFIRMATION = "reservation-confirmation";
-    private static final String TEMPLATE_FOR_RESERVATION_REMINDER = "reservation-reminder";
-
-    @Value("${backend.base.url}")
-    private String backendBaseUrl;
-
     @Override
     public String buildEmailContent(String name, String token, Reservation reservation, EmailSubject subject) {
-        String link = null;
-        String template;
-        switch (subject) {
-            case CONFIRM_EMAIL -> {
-                link = backendBaseUrl + TOKEN_CONFIRMATION_EMAIL_URL + token;
-                template = TEMPLATE_FOR_CONFIRM_EMAIL;
-            }
-            case FORGOT_PASSWORD -> {
-                link = backendBaseUrl + TOKEN_RESET_PASSWORD_URL + token;
-                template = TEMPLATE_FOR_FORGOT_PASSWORD_EMAIL;
-            }
-            case RESERVATION_CONFIRMATION -> template = TEMPLATE_FOR_RESERVATION_CONFIRMATION;
-            case RESERVATION_REMINDER -> template = TEMPLATE_FOR_RESERVATION_REMINDER;
-            default -> throw new IllegalArgumentException("Wrong email subject");
-        }
+        String link = subject.getLink(token);
+        String template = subject.getTemplate();
 
         Context ctx = new Context();
         ctx.setVariable("name", name);
